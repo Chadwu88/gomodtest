e88cea3f8b15912daddb4179e8f77cab17f37853
chadwu <chadwu@tencent.com>
2020-03-22 10:15:13 +0800

    main.go

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Chadwu88/gomodtest/morestrings"
+)
+
+func main() {
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
+}
